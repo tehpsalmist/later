62428a9eeca73c4c7d85e55ac0c23c7e627bc347
tehpsalmist <bstewardcodes@gmail.com>
2019-11-04 01:41:20 -0500

    400 if no refresh token in body

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,6 +4,10 @@ const refreshToken = (req, res) => {
   const refresh_token = req.body.refreshToken
   const client_secret = process.env.LATER_ON_AUTH0_CLIENT_SECRET
 
+  if (!refresh_token) {
+    return res.status(400).json({ message: 'missing refreshToken in POST body' })
+  }
+
   auth0.refreshToken({ refresh_token, client_secret }, (error, authData) => {
     if (error || !authData) {
       return res.status(500).json({ error: error || 'unable to fetch token' })
